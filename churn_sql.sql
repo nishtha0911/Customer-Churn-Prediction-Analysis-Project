d55b7b1491e1ac31578ee7ee599436a6718b1fe6
Nishtha Mittal <123964647+nishtha0911@users.noreply.github.com>
2025-08-02 23:22:04 +0530

    Add files via upload

---
+++ churn_sql.sql
@@ -0,0 +1,123 @@
+create database db_churn;
+use db_churn;
+select * from stg_churn;
+EXEC sp_help 'stg_churn'; --describe the data   
+
+select Gender,count(Gender) as TotalCount,
+count(gender)*100.0 /(select count(*) from stg_churn) as percentage  --percentage distributution
+from stg_churn group by Gender;
+
+select Contract,count(Contract) as TotalCount,
+count(Contract)*100.0 /(select count(*) from stg_churn) as percentage
+from stg_churn group by Contract;
+
+-- Out of all customers, how many are still active, how many have left (churned), and 
+-- how many are newly joined — both in count and percentage terms
+
+select Customer_Status,count(Customer_Status) as TotalCount,
+count(Customer_Status)*100.0 /(select count(*) from stg_churn) as percentage
+from stg_churn group by Customer_Status;
+
+
+--“How much revenue came from each customer group, how many customers were there in each, and 
+--what percent of total revenue each group contributed?”
+
+select Customer_Status,count(customer_status) as TotalCount,sum(Total_Revenue) as TotalRev,
+-- Out of 100% of total revenue, how much came from customers in this Customer_Status group
+sum(Total_Revenue)*100.0 /(select sum(Total_Revenue) from stg_churn) as revpercentage 
+from stg_churn group by Customer_Status;
+
+-- to know which state has higher contribution in our entire data
+select State,count(State) as totalCount,
+count(State)*100.0/(select count(*) from stg_churn) as statePer
+from stg_churn group by State order by statePer desc;
+
+select distinct internet_type from stg_churn;
+
+
+-- Data Exploration – Check Nulls
+SELECT 
+    SUM(CASE WHEN Customer_ID IS NULL THEN 1 ELSE 0 END) AS Customer_ID_Null_Count,
+    SUM(CASE WHEN Gender IS NULL THEN 1 ELSE 0 END) AS Gender_Null_Count,
+    SUM(CASE WHEN Age IS NULL THEN 1 ELSE 0 END) AS Age_Null_Count,
+    SUM(CASE WHEN Married IS NULL THEN 1 ELSE 0 END) AS Married_Null_Count,
+    SUM(CASE WHEN State IS NULL THEN 1 ELSE 0 END) AS State_Null_Count,
+    SUM(CASE WHEN Number_of_Referrals IS NULL THEN 1 ELSE 0 END) AS Number_of_Referrals_Null_Count,
+    SUM(CASE WHEN Tenure_in_Months IS NULL THEN 1 ELSE 0 END) AS Tenure_in_Months_Null_Count,
+    SUM(CASE WHEN Value_Deal IS NULL THEN 1 ELSE 0 END) AS Value_Deal_Null_Count,
+    SUM(CASE WHEN Phone_Service IS NULL THEN 1 ELSE 0 END) AS Phone_Service_Null_Count,
+    SUM(CASE WHEN Multiple_Lines IS NULL THEN 1 ELSE 0 END) AS Multiple_Lines_Null_Count,
+    SUM(CASE WHEN Internet_Service IS NULL THEN 1 ELSE 0 END) AS Internet_Service_Null_Count,
+    SUM(CASE WHEN Internet_Type IS NULL THEN 1 ELSE 0 END) AS Internet_Type_Null_Count,
+    SUM(CASE WHEN Online_Security IS NULL THEN 1 ELSE 0 END) AS Online_Security_Null_Count,
+    SUM(CASE WHEN Online_Backup IS NULL THEN 1 ELSE 0 END) AS Online_Backup_Null_Count,
+    SUM(CASE WHEN Device_Protection_Plan IS NULL THEN 1 ELSE 0 END) AS Device_Protection_Plan_Null_Count,
+    SUM(CASE WHEN Premium_Support IS NULL THEN 1 ELSE 0 END) AS Premium_Support_Null_Count,
+    SUM(CASE WHEN Streaming_TV IS NULL THEN 1 ELSE 0 END) AS Streaming_TV_Null_Count,
+    SUM(CASE WHEN Streaming_Movies IS NULL THEN 1 ELSE 0 END) AS Streaming_Movies_Null_Count,
+    SUM(CASE WHEN Streaming_Music IS NULL THEN 1 ELSE 0 END) AS Streaming_Music_Null_Count,
+    SUM(CASE WHEN Unlimited_Data IS NULL THEN 1 ELSE 0 END) AS Unlimited_Data_Null_Count,
+    SUM(CASE WHEN Contract IS NULL THEN 1 ELSE 0 END) AS Contract_Null_Count,
+    SUM(CASE WHEN Paperless_Billing IS NULL THEN 1 ELSE 0 END) AS Paperless_Billing_Null_Count,
+    SUM(CASE WHEN Payment_Method IS NULL THEN 1 ELSE 0 END) AS Payment_Method_Null_Count,
+    SUM(CASE WHEN Monthly_Charge IS NULL THEN 1 ELSE 0 END) AS Monthly_Charge_Null_Count,
+    SUM(CASE WHEN Total_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Charges_Null_Count,
+    SUM(CASE WHEN Total_Refunds IS NULL THEN 1 ELSE 0 END) AS Total_Refunds_Null_Count,
+    SUM(CASE WHEN Total_Extra_Data_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Extra_Data_Charges_Null_Count,
+    SUM(CASE WHEN Total_Long_Distance_Charges IS NULL THEN 1 ELSE 0 END) AS Total_Long_Distance_Charges_Null_Count,
+    SUM(CASE WHEN Total_Revenue IS NULL THEN 1 ELSE 0 END) AS Total_Revenue_Null_Count,
+    SUM(CASE WHEN Customer_Status IS NULL THEN 1 ELSE 0 END) AS Customer_Status_Null_Count,
+    SUM(CASE WHEN Churn_Category IS NULL THEN 1 ELSE 0 END) AS Churn_Category_Null_Count,
+    SUM(CASE WHEN Churn_Reason IS NULL THEN 1 ELSE 0 END) AS Churn_Reason_Null_Count
+FROM stg_Churn;
+
+-- now replace nulls from each column and insert data into new table
+
+SELECT
+    Customer_ID,
+    Gender,
+    Age,
+    Married,
+    State,
+    Number_of_Referrals,
+    Tenure_in_Months,
+    ISNULL(Value_Deal, 'None') AS Value_Deal,
+    Phone_Service,
+    ISNULL(Multiple_Lines, 'No') AS Multiple_Lines,
+    Internet_Service,
+    ISNULL(Internet_Type, 'None') AS Internet_Type,
+    ISNULL(Online_Security, 'No') AS Online_Security,
+    ISNULL(Online_Backup, 'No') AS Online_Backup,
+    ISNULL(Device_Protection_Plan, 'No') AS Device_Protection_Plan,
+    ISNULL(Premium_Support, 'No') AS Premium_Support,
+    ISNULL(Streaming_TV, 'No') AS Streaming_TV,
+    ISNULL(Streaming_Movies, 'No') AS Streaming_Movies,
+    ISNULL(Streaming_Music, 'No') AS Streaming_Music,
+    ISNULL(Unlimited_Data, 'No') AS Unlimited_Data,
+    Contract,
+    Paperless_Billing,
+    Payment_Method,
+    Monthly_Charge,
+    Total_Charges,
+    Total_Refunds,
+    Total_Extra_Data_Charges,
+    Total_Long_Distance_Charges,
+    Total_Revenue,
+    Customer_Status,
+    ISNULL(Churn_Category, 'Others') AS Churn_Category,
+    ISNULL(Churn_Reason, 'Others') AS Churn_Reason
+	into [db_churn].[dbo].[prod_churn]
+	from [db_churn].[dbo].[stg_churn];
+
+
+
+-- view create ->view is virtual table that displays data from one or more table. It does not have physical presence on the db itself
+Create View vw_churndata as 
+	select * from prod_churn where Customer_Status in ('churned','stayed'); 
+select * from vw_churndata;
+
+
+create view vw_joindata as
+Select * from prod_churn where Customer_Status ='Joined';
+SELECT @@SERVERNAME AS Server_Name;
+
